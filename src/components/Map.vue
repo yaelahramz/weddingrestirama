9d6ee128d11b2fe36c7fa2e0839485e58a251319
yaelahramz <37044225+yaelahramz@users.noreply.github.com>
2024-04-17 21:01:12 +0900

    hapus maps

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -33,61 +33,6 @@
   </div>
 </template>
 
-<script>
-
-import VueDaumMap from 'vue-daum-map'
-
-export default {
-  name: "Map",
-  components: {VueDaumMap},
-  data() {
-    return {
-      scrollX: 0,
-      width: 320,
-      appKey: '13820f5d51d54f2bfb5203278b7bf8b9', // 테스트용 appkey
-      center: {lat: 37.4987, lng: 127.0316}, // 지도의 중심 좌표
-      level: 3, // 지도의 레벨(확대, 축소 정도),
-      mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
-      libraries: ["drawing"], // 추가로 불러올 라이브러리
-      map: null, // 지도 객체. 지도가 로드되면 할당됨.
-      daum: null, // 다음 API 객체. 지도가 로드되면 할당됨.
-      //////////////
-      tmapUrl: "",
-      kakaoTaxiUrl: "",
-      navermapUrl: "",
-      kakaomapUrl: ""
-
-    };
-  },
-  mounted() {
-    this.makeUrls()
-  },
-  methods: {
-    // 지도가 로드 완료되면 load 이벤트 발생
-    onLoad(map, daum) {
-      this.map = map;
-      this.daum = daum;
-
-      // 지도를 클릭한 위치에 표출할 마커입니다
-      var marker = new kakao.maps.Marker({
-        // 지도 중심좌표에 마커를 생성합니다
-        position: map.getCenter()
-      });
-      // 지도에 마커를 표시합니다
-      marker.setMap(map);
-    },
-    makeUrls() {
-      // 장소데이터의 이름정보 불러온 뒤
-      const locationName = "힐링페이퍼"
-      this.tmapUrl = "tmap://search?name=" + locationName
-      this.kakaoTaxiUrl = "https://t.kakao.com/launch?type=taxi&amp;dest_lat=37.49878007763176&amp;dest_lng=127.03170076652506&amp;ref=localweb"
-      this.navermapUrl = "nmap://search?query=" + locationName + "&appname=kimyoon21.github.io/wedding"
-      this.kakaomapUrl = "kakaomap://search?q=" + locationName
-    }
-  }
-};
-</script>
-
 <style lang="scss" scoped>
 
 .map {

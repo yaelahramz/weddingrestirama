418a888900cc5492e9720c2dd3147488c967c665
yaelahramz <37044225+yaelahramz@users.noreply.github.com>
2024-04-17 21:28:51 +0900

    form update

--- src/components/Funding.vue
+++ src/components/Funding.vue
@@ -39,6 +39,18 @@ export default {
 };
 </script>
 
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
 <style lang="scss" scoped>
 .funding {
   padding-top: 48px;
